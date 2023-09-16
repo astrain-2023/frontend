216fdb10d3e9b60cb73654f5acfc86374038d85f
Luca Christen <lucachristen94@gmail.com>
2023-09-16 18:56:51 +0200

    Update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'astrAIn',
+  title: 'JamHero',
   description: 'Predict and prevent traffic congestions by rewarding drivers for congestion-limiting behavior',
 }
 
