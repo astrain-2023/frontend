a00ae8b8673aed6e67eb47a39acc998721ca930f
Luca Christen <lucachristen94@gmail.com>
2023-09-16 20:58:07 +0200

    Make PWA fullscreen on iOS

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="manifest" href="/manifest.json" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
